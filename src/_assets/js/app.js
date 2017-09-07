2335192f8150fd4067cc626c9319cfe32a688927
HaoZeke <driger633@gmail.com>
2017-09-07 07:51:32 +0530

    js: Add smooth scrolling

--- src/_assets/js/app.js
+++ src/_assets/js/app.js
@@ -1,3 +1,7 @@
-//=require vendor/umbrella.min.js
+//= require vendor/umbrella.min.js
+//= require vendor/smooth-scroll.min.js
 //= require bulma/nav.js
 //= require bulma/modal.js
+
+
+var scroll = new SmoothScroll('a[href*="#"]');
