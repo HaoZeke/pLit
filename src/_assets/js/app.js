f36e2b5e55d4749045ba03a13c601d8429b148c7
HaoZeke <driger633@gmail.com>
2017-09-07 07:05:14 +0530

    js: Add umbrella to fix modals

--- src/_assets/js/app.js
+++ src/_assets/js/app.js
@@ -1,2 +1,3 @@
+//=require vendor/umbrella.min.js
 //= require bulma/nav.js
 //= require bulma/modal.js
--- src/_assets/js/bulma/modal.js
+++ src/_assets/js/bulma/modal.js
@@ -1,10 +1,23 @@
-var buttons = Array.from(document.getElementsByClassName('modal-button'));
+// var buttons = Array.from(document.getElementsByClassName('modal-button'));
 
-for (var i = 0; i < buttons.length; i++) {
-  var button = buttons[i];
-  var target = document.querySelector(button.dataset.target);
+// for (var i = 0; i < buttons.length; i++) {
+//   var button = buttons[i];
+//   var target = document.querySelector(button.dataset.target);
   
-  button.addEventListener("click", function(e) {
-    target.classList.toggle('is-active');
-  }, false);
-}
+//   button.addEventListener("click", function(e) {
+//     target.classList.toggle('is-active');
+//   }, false);
+// }
+
+/*u('.modal-button').on('click',function(e) {
+u(this).toggleClass('is-active')})
+
+u('.modal-button').on('click',function(e) {
+u('[data-target]')
+u(this).toggleClass('meeee')}).array()
+*/
+
+
+u('.modal-button').on('click',function(e) {
+var temp=u(this).data('target')
+u(temp).toggleClass('is-active')})
